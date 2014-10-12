7e5d6f43092d514b3b30b78e85d099a5b2ccfd4f
Alyssa Diaz <alyssa@devbootcamp.com>
2014-10-12 02:08:09 -0500

    Classifieds pairing

--- app/controllers/classifieds.rb
+++ app/controllers/classifieds.rb
@@ -3,3 +3,30 @@ get '/classifieds' do
   erb :"classifieds/index"
 end
 
+get '/classifieds/new' do
+  @classified = Classified.new
+  @categories = Category.all
+  erb :"classifieds/new"
+end
+
+post '/classifieds/new' do
+  @classified = Classified.new(
+    title: params[:classified][:title],
+    description: params[:classified][:description],
+    condition: params[:classified][:condition],
+    location: params[:classified][:location],
+    zip_code: params[:classified][:zip_code],
+    price: params[:classified][:price].to_f,
+    category: Category.find(params[:classified][:category]),
+    user: current_user)
+  if @classified.save
+    redirect '/classifieds'
+  else
+    erb :"classifieds/new"
+  end
+end
+
+get '/classifieds/:id' do
+  @classified = Classified.find(params[:id])
+  erb :"classifieds/show"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-user = User.create!(first_name: "Danielle", last_name: "Dirks", username: "danisaurus", email: "danidirks@gmail.com", password: "donuts" )
+user = User.create!(first_name: "Danielle",
+                    last_name: "Dirks",
+                    username: "danisaurus",
+                    email: "danidirks@gmail.com",
+                    password: "donuts" )
 
 15.times do
   User.create(first_name: Faker::Name.first_name,
@@ -10,19 +14,17 @@ user = User.create!(first_name: "Danielle", last_name: "Dirks", username: "danis
 end
 
 15.times do
-  Category.create(
-    name:        Faker::Lorem.words(2).join(" ").upcase,
-    description: Faker::Lorem.paragraph)
+  Category.create(name: Faker::Lorem.words(2).join(" ").upcase,
+                  description: Faker::Lorem.paragraph)
 end
 
 15.times do
-  Classified.create(
-    title:       Faker::Company.name,
-    description: Faker::Company.catch_phrase,
-    condition:   Faker::Company.bs,
-    location:    Faker::Address.street_address,
-    zip_code:    Faker::Address.zip_code,
-    price:       Faker::Commerce.price,
-    user_id:     User.all.sample.id,
-    category_id: Category.all.sample.id)
+  Classified.create(title: Faker::Company.name,
+                    description: Faker::Company.catch_phrase,
+                    condition:   Faker::Company.bs,
+                    location:    Faker::Address.street_address,
+                    zip_code:    Faker::Address.zip_code,
+                    price:       Faker::Commerce.price,
+                    user_id:     User.all.sample.id,
+                    category_id: Category.all.sample.id)
 end
